66d655b8f9c12bb2f963c0f0a9fbfcbdfe40b834
Jonathon Leight <jonathon.leight@jleight.com>
2014-09-30 21:19:13 -0400

    Setting up basic project and Gruntfile.

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    browserify: {
+      options: {
+        debug: true,
+        transform: ['reactify']
+      },
+      dev: {
+        src: ['src/scripts/site.js'],
+        dest: 'dist/scripts/site.js'
+      }
+    },
+
+    replace: {
+      root: {
+        src: ['src/*.html'],
+        dest: 'dist/',
+        replacements: [{
+          from: '%root%',
+          to: '/d3ps'
+        }]
+      }
+    },
+
+    copy: {
+      styles: {
+        cwd: 'src/styles',
+        src: '**/*',
+        dest: 'dist/styles',
+        expand: true
+      }
+    },
+
+    watch: {
+      react: {
+        files: ['src/scripts/**/*.js'],
+        tasks: ['browserify']
+      },
+      html: {
+        files: ['src/**/*.html'],
+        tasks: ['replace']
+      },
+      assets: {
+        files: ['src/styles/**/*'],
+        tasks: ['copy']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-text-replace');
+
+  grunt.registerTask('default', ['browserify', 'replace', 'copy']);
+};
